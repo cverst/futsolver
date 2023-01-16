75f1fd02f4730aa0c4666b42d02eb4fbe2759f87
Corstiaen Versteegh <corstiaen.versteegh@afry.com>
2023-01-16 16:14:09 +0100

    First version of chemistry implemented but with mistakes; also much too slow. Needs more work.

--- main/run.py
+++ main/run.py
@@ -1,32 +1,44 @@
 import random
 from solver import SBCSolver
 from utils.data import get_data
-import datetime
-
+import time
 
 DATA_PATH = "../data/Fifa 23 Fut Players.csv"
 
 CONSTRAINTS = {
-    "minimum_team_rating": 75,
+    "minimum_team_rating": 70,
+    # "minimum_chemistry": 8,
+    # "minimize": True,
+    "maximum_cost": 2200,
 }
 
+FORMATION = "442"
 
 def run():
-    print("Start time:", datetime.datetime.now().strftime("%H:%M:%S"))
+    print()
+    print("Start time:", time.strftime("%H:%M:%S", time.localtime()))
 
     data = get_data(DATA_PATH)
 
-    # Randomly select 5% of rows from data to speed up testing
-    data = random.sample(data, int(len(data) * 0.1))
+    # TODO: FIGURE OUT BETTER WAY OF LIMITING SEARCH SPACE. MAYBE CALCULATE ALL RATING COMBINATIONS AND THEN FILTER OUT WHERE MEDIAN PRICES ARE LARGER THAN NAIVE MEDIAN PRICE?
+    # Remove players more than 5 rating points above the minimum team rating
+    data = [player_data for player_data in data if player_data[2] <= CONSTRAINTS["minimum_team_rating"] + 5]
 
-    sbc_solver = SBCSolver(data, formation="442", minimize=False)
+    # Randomly select 5% of rows from data to speed up testing
+    random.seed(42)
+    # data = random.sample(data, int(len(data) * 0.02))
+
+    print("Number of players:", len(data))
+
+    sbc_solver = SBCSolver(data, formation=FORMATION)
 
     sbc_solver.build(CONSTRAINTS)
 
     # sbc_solver.solve(time_limit=10)
     sbc_solver.solve()
 
-    print("End time:", datetime.datetime.now().strftime("%H:%M:%S"))
+    print("End time:", time.strftime("%H:%M:%S", time.localtime()))
+    print()
 
 
 if __name__ == "__main__":
--- main/solver.py
+++ main/solver.py
@@ -2,6 +2,7 @@ from ortools.sat.python import cp_model
 import json
 from typing import List, Union
 from utils.stats import team_rating
+import time
 
 
 FORMATION_FILE = "../src/formations.json"
@@ -12,17 +13,16 @@ class SBCSolver:
         self,
         data: List[List[Union[int, str, list]]],
         formation: str = "442",
-        minimize: bool = False,
     ) -> None:
 
         self.data = data
         self.formation = formation
-        self.minimize = minimize
 
         self.team_size = None
         self.formation_list = None
 
         self._variables_id = None
+        self._variables_player_position = None
         self._constraints = None
 
         self.model = cp_model.CpModel()
@@ -47,8 +47,12 @@ class SBCSolver:
 
         Key/Value Pairs:
             minimum_team_rating (int): Minimum team rating.
-            max_cost (int): Maximum cost of the team.
+            minimum_chemistry (int): Minimum chemistry.
+            minimize (bool): Whether to minimize the cost of the team.
+            maximum_cost (int): Maximum cost of the team.
         """
+
+        start_time = time.time()
 
         if constraints is None:
             constraints = {}
@@ -57,19 +61,26 @@ class SBCSolver:
         self._build_base_constraint()
 
         if "minimum_team_rating" in constraints:
-            self._build_rating_constraint(constraints["minimum_team_rating"])
+            self._build_rating_constraint()
 
-        if "max_cost" in constraints:
-            self._build_cost_constraint(constraints["max_cost"])
-        else:
+        if "minimum_chemistry" in constraints:
+            self._build_chemistry_constraint()
+
+        if "maximum_cost" in constraints:
+            self._build_cost_constraint()
+
+        if "minimize" in constraints:
             self._build_minimization()
+
+        end_time = time.time()
+        print("Build time:", end_time - start_time)
 
     def _build_base_constraint(self) -> None:
 
         # Variables
         self._variables_id = {}
-        for ind, player_data in enumerate(self.data):
-            self._variables_id[ind] = self.model.NewBoolVar(
+        for index, player_data in enumerate(self.data):
+            self._variables_id[index] = self.model.NewBoolVar(
                 "id_{}".format(player_data[0])
             )
 
@@ -86,7 +97,7 @@ class SBCSolver:
             ]
             self.model.Add(sum(single_player_variables) <= 1)
 
-    def _build_rating_constraint(self, minimum_team_rating: int) -> None:
+    def _build_rating_constraint(self) -> None:
 
         # Constants
         all_ratings = [player_data[2] for player_data in self.data]
@@ -103,18 +114,18 @@ class SBCSolver:
         variables_excess_rating = {}
         variables_max_excess_rating = {}
         variables_max_excess_rating_masked = {}
-        for ind, player_data in enumerate(self.data):
-            variables_excess_rating[ind] = self.model.NewIntVar(
+        for index, player_data in enumerate(self.data):
+            variables_excess_rating[index] = self.model.NewIntVar(
                 -diff_rating * self.team_size,
                 diff_rating * self.team_size,
                 "excess_rating_{}".format(player_data[0]),
             )
-            variables_max_excess_rating[ind] = self.model.NewIntVar(
+            variables_max_excess_rating[index] = self.model.NewIntVar(
                 0,
                 diff_rating * self.team_size,
                 "max_excess_rating_{}".format(player_data[0]),
             )
-            variables_max_excess_rating_masked[ind] = self.model.NewIntVar(
+            variables_max_excess_rating_masked[index] = self.model.NewIntVar(
                 0,
                 diff_rating * self.team_size,
                 "max_excess_rating_masked_{}".format(player_data[0]),
@@ -122,48 +133,289 @@ class SBCSolver:
 
         # Constraints
         # Establish base rating
-        self.model.Add(
-            sum(
-                [
-                    selected * rating
-                    for selected, rating in zip(
-                        self._variables_id.values(), all_ratings
-                    )
-                ]
-            )
-            == variable_base_rating
+        base_rating = sum(
+            [
+                selected * rating
+                for selected, rating in zip(
+                    self._variables_id.values(), all_ratings
+                )
+            ]
         )
+        self.model.Add(base_rating == variable_base_rating)
         # Establish excess ratings
-        for ind, var in enumerate(self._variables_id.values()):
-            player_rating = self.data[ind][2]
+        for index, var in enumerate(self._variables_id.values()):
+            player_rating = self.data[index][2]
             self.model.Add(
-                variables_excess_rating[ind]
+                variables_excess_rating[index]
                 == player_rating * self.team_size - variable_base_rating
             ).OnlyEnforceIf(var)
             # Remove negative excess ratings
             self.model.AddMaxEquality(
-                variables_max_excess_rating[ind],
-                [variables_excess_rating[ind], 0],
+                variables_max_excess_rating[index],
+                [variables_excess_rating[index], 0],
             )
             # Mask excess rating with selected players
             self.model.AddMultiplicationEquality(
-                variables_max_excess_rating_masked[ind],
-                [variables_max_excess_rating[ind], var],
+                variables_max_excess_rating_masked[index],
+                [variables_max_excess_rating[index], var],
             )
         # Add excess ratings of selected players to base rating
         self.model.Add(
             variable_base_rating * self.team_size
             + sum(variables_max_excess_rating_masked.values())
-            >= minimum_team_rating * self.team_size**2
+            >= self._constraints["minimum_team_rating"] * self.team_size**2
         )
 
     def _build_chemistry_constraint(self) -> None:
-        pass
+        # TODO: CREATE BOOLEANS FOR PREFERRED PLAYER POSITIONS HERE AND NOT IN GET_DATA FUNCTION. SEE HOW CLUBS ARE CALCULATED IN CHEMISTRY CONSTRAINT
+        # TODO: REALLY NEEDS TO SPEED UP. FIND A SOLUTION WITH FEWER CONSTRAINTS.
+        # Constants
+        MAP_POSITION_TO_COLUMN = {
+            "CAM": 10,
+            "CB": 11,
+            "CDM": 12,
+            "CF": 13,
+            "CM": 14,
+            "GK": 15,
+            "LB": 16,
+            "LM": 17,
+            "LW": 18,
+            "LWB": 19,
+            "RB": 20,
+            "RM": 21,
+            "RW": 22,
+            "RWB": 23,
+            "ST": 24,
+        }
+        MAX_CHEMISTRY_PER_PLAYER = 3
+
+        # Variables
+        self._variables_player_position = {}
+        variables_player_position_correct = {}
+        variables_potential_player_chemistry_raw = {}
+        variables_potential_player_chemistry = {}
+        variables_player_chemistry = {}
+        variables_player_chemistry_selected = {}
+        for index, player_data in enumerate(self.data):
+            variables_player_position_correct[index] = self.model.NewBoolVar(
+                "player_position_correct_{}".format(player_data[0])
+            )
+            for position in self.formation_list:
+                self._variables_player_position[
+                    (index, position)
+                ] = self.model.NewBoolVar(
+                    "player_position_{}_{}".format(player_data[0], position)
+                )
+            variables_potential_player_chemistry_raw[
+                index
+            ] = self.model.NewIntVar(
+                0,
+                MAX_CHEMISTRY_PER_PLAYER * 3,
+                "potential_player_chemistry_raw",
+            )
+            variables_potential_player_chemistry[index] = self.model.NewIntVar(
+                0, MAX_CHEMISTRY_PER_PLAYER, "potential_player_chemistry"
+            )
+            variables_player_chemistry[index] = self.model.NewIntVar(
+                0, MAX_CHEMISTRY_PER_PLAYER, "player_chemistry"
+            )
+            variables_player_chemistry_selected[index] = self.model.NewIntVar(
+                0, MAX_CHEMISTRY_PER_PLAYER, "player_chemistry_selected"
+            )
+        variables_club_count = {}
+        variables_club_chemistry_division = {}
+        variables_club_chemistry = {}
+        clubs = list(set([player_data[7] for player_data in self.data]))
+        for club in clubs:
+            variables_club_count[club] = self.model.NewIntVar(
+                0, self.team_size, "club_count_{}".format(club)
+            )
+            variables_club_chemistry_division[club] = self.model.NewIntVar(
+                0, 4, "club_chemistry_division_{}".format(club)
+            )
+            variables_club_chemistry[club] = self.model.NewIntVar(
+                0, MAX_CHEMISTRY_PER_PLAYER, "club_chemistry"
+            )
+        variables_league_count = {}
+        variables_league_chemistry_division = {}
+        variables_league_chemistry = {}
+        leagues = list(set([player_data[8] for player_data in self.data]))
+        for league in leagues:
+            variables_league_count[league] = self.model.NewIntVar(
+                0, self.team_size, "league_count_{}".format(league)
+            )
+            variables_league_chemistry_division[league] = self.model.NewIntVar(
+                0, 4, "league_chemistry_division_{}".format(league)
+            )
+            variables_league_chemistry[league] = self.model.NewIntVar(
+                0, MAX_CHEMISTRY_PER_PLAYER, "league_chemistry"
+            )
+        variables_country_count = {}
+        variables_country_chemistry_division = {}
+        variables_country_chemistry = {}
+        countries = list(set([player_data[9] for player_data in self.data]))
+        for country in countries:
+            variables_country_count[country] = self.model.NewIntVar(
+                0, self.team_size, "country_count_{}".format(country)
+            )
+            variables_country_chemistry_division[
+                country
+            ] = self.model.NewIntVar(
+                0, 4, "country_chemistry_division_{}".format(country)
+            )
+            variables_country_chemistry[country] = self.model.NewIntVar(
+                0, MAX_CHEMISTRY_PER_PLAYER, "country_chemistry"
+            )
+
+        # Constraints
+        # Make sure that a selected player has a position
+        for index, var in enumerate(self._variables_id.values()):
+            n_players_selected_per_position = sum(
+                [
+                    self._variables_player_position[(index, position)]
+                    for position in self.formation_list
+                ]
+            )
+            self.model.Add(n_players_selected_per_position == 1).OnlyEnforceIf(
+                var
+            )
+        # Make sure each position has exactly one player
+        for position in self.formation_list:
+            n_positions_selected_per_player = sum(
+                [
+                    self._variables_player_position[(id, position)]
+                    for id in self._variables_id.keys()
+                ]
+            )
+            self.model.Add(n_positions_selected_per_player == 1)
+        # Calculate if chemistry should be counted
+        for index, (id, var) in enumerate(self._variables_id.items()):
+            for position in self.formation_list:
+                self.model.Add(
+                    variables_player_position_correct[index]
+                    == self._variables_player_position[(id, position)]
+                    * self.data[index][
+                        MAP_POSITION_TO_COLUMN[position.split("_")[0]]
+                    ]
+                ).OnlyEnforceIf(
+                    self._variables_player_position[(id, position)]
+                )
+        # Count number of players per club
+        # [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3] -> chemistry
+        # [0, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4] -> (2 * (n_players + 1)) // 5
+        for club, var in variables_club_count.items():
+            club_bool = [player_data[7] == club for player_data in self.data]
+            players_selected_for_club = sum(
+                [
+                    var * b
+                    for var, b in zip(self._variables_id.values(), club_bool)
+                ]
+            )
+            self.model.Add((players_selected_for_club + 1) * 2 == var)
+            self.model.AddDivisionEquality(
+                variables_club_chemistry_division[club],
+                var,
+                5,
+            )
+            self.model.AddMinEquality(
+                variables_club_chemistry[club],
+                [
+                    variables_club_chemistry_division[club],
+                    MAX_CHEMISTRY_PER_PLAYER,
+                ],
+            )
+        # Count number of players per league
+        # [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3] -> chemistry
+        # [0, 0, 1, 1, 2, 2, 2, 3, 3, 4, 4] -> (2 * n_players) // 5
+        for league, var in variables_league_count.items():
+            league_bool = [
+                player_data[8] == league for player_data in self.data
+            ]
+            players_selected_for_league = sum(
+                [
+                    var * b
+                    for var, b in zip(self._variables_id.values(), league_bool)
+                ]
+            )
+            self.model.Add((players_selected_for_league) * 2 == var)
+            self.model.AddDivisionEquality(
+                variables_league_chemistry_division[league],
+                var,
+                5,
+            )
+            self.model.AddMinEquality(
+                variables_league_chemistry[league],
+                [
+                    variables_league_chemistry_division[league],
+                    MAX_CHEMISTRY_PER_PLAYER,
+                ],
+            )
+        # Count number of players per country
+        # [0, 0, 0, 1, 1, 2, 2, 2, 3, 3, 3] -> chemistry
+        # [0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4] -> (n_players + 1) // 3
+        for country, var in variables_country_count.items():
+            country_bool = [
+                player_data[9] == country for player_data in self.data
+            ]
+            players_selected_for_country = sum(
+                [
+                    var * b
+                    for var, b in zip(
+                        self._variables_id.values(), country_bool
+                    )
+                ]
+            )
+            self.model.Add((players_selected_for_country + 1) == var)
+            self.model.AddDivisionEquality(
+                variables_country_chemistry_division[country],
+                var,
+                3,
+            )
+            self.model.AddMinEquality(
+                variables_country_chemistry[country],
+                [
+                    variables_country_chemistry_division[country],
+                    MAX_CHEMISTRY_PER_PLAYER,
+                ],
+            )
+        # Calculate potential chemistry per player
+        for index, var in enumerate(self._variables_id.values()):
+            self.model.Add(
+                variables_potential_player_chemistry_raw[index]
+                == variables_club_chemistry[self.data[index][7]]
+                + variables_league_chemistry[self.data[index][8]]
+                + variables_country_chemistry[self.data[index][9]]
+            ).OnlyEnforceIf(var)
+            self.model.AddMinEquality(
+                variables_potential_player_chemistry[index],
+                [
+                    variables_potential_player_chemistry_raw[index],
+                    MAX_CHEMISTRY_PER_PLAYER,
+                ],
+            )
+        # Calculate chemistry
+        for index, var in enumerate(self._variables_id.values()):
+            self.model.AddMultiplicationEquality(
+                variables_player_chemistry[index],
+                [
+                    variables_potential_player_chemistry[index],
+                    variables_player_position_correct[index],
+                ],
+            )
+            self.model.AddMultiplicationEquality(
+                variables_player_chemistry_selected[index],
+                [
+                    variables_player_chemistry[index],
+                    var,
+                ],
+            )
+        self.model.Add(sum(variables_player_chemistry_selected.values()) >= self._constraints["minimum_chemistry"])
+
 
     def _build_feature_constraint(self) -> None:
         pass
 
-    def _build_cost_constraint(self, max_cost: int) -> None:
+    def _build_cost_constraint(self) -> None:
         player_costs = [player_data[6] for player_data in self.data]
         self.model.Add(
             sum(
@@ -174,10 +426,11 @@ class SBCSolver:
                     )
                 ]
             )
-            <= max_cost
+            <= self._constraints["maximum_cost"]
         )
 
     def _build_minimization(self) -> None:
+        # TODO: TEST IF MINIMIZATION CAN BE TIME LIMITED
         player_costs = [player_data[6] for player_data in self.data]
         self.model.Minimize(
             sum(
@@ -203,7 +456,13 @@ class SBCSolver:
         if time_limit is not None:
             self.solver.parameters.max_time_in_seconds = time_limit
 
+        start_time = time.time()
+
         status = self.solver.Solve(self.model)
+
+        end_time = time.time()
+        print("Solve time:", end_time - start_time)
+        print()
 
         if status == cp_model.OPTIMAL:
             print("Optimal solution found!")
@@ -234,27 +493,34 @@ class SBCSolver:
                         for s, l in zip(player_data, longest_strings)
                     )
                 )
+            print()
 
-            # print(
-            #     "Selected:",
-            #     [
-            #         solver.Value(v)
-            #         for v, m in zip(
-            #             variables_playerpositioncorrect.values(), mask
-            #         )
-            #         if m
-            #     ],
-            # )
-            # print(
-            #     "Chemistry:",
-            #     [
-            #         solver.Value(v)
-            #         for v, m in zip(
-            #             variables_playerchemistry.values(), mask
-            #         )
-            #         if m
-            #     ],
-            # )
+            if self._variables_player_position is None:
+                print("No position constraints.")
+            else:
+                players_by_position = [
+                    key
+                    for key, var in self._variables_player_position.items()
+                    if self.solver.Value(var)
+                ]
+                players_by_position = [
+                    list(item)[::-1] for item in players_by_position
+                ]
+                players_by_position = sorted(
+                    players_by_position,
+                    key=lambda x: self.formation_list.index(x[0]),
+                )
+                players_by_position = [
+                    (pos, self.data[index][1])
+                    for pos, index in players_by_position
+                ]
+                for pos, player in players_by_position:
+                    print(
+                        pos.rjust(longest_strings[0])
+                        + " : "
+                        + player.ljust(longest_strings[1])
+                    )
+                print()
 
             print("Total cost: {}".format(total_cost))
             print("Team rating: {}".format(team_rating(ratings)))
